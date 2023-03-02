0500c4c6caa6c4e6ddffe9f7cbb0d03e32804652
Mykhailo <126561564+Helixoz@users.noreply.github.com>
2023-03-02 11:27:15 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+       byte a = 8;
+       short b = 10;
+       int c = 15;
+       float f = 25f;
+       long d = 20;
+       double s = 13d;
+
+        System.out.println("Result + : " + (a + b));
+        System.out.println("Result - : " + (c - f));
+        System.out.println("Result * : " + (b * s));
+        System.out.println("Result / : " + (s / d));
+    }
+}
