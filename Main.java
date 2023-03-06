ce9b447824e7788050b4c52d29c8cc622abafd45
Mykhailo <126561564+Helixoz@users.noreply.github.com>
2023-03-06 17:14:01 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        
        byte a = 8;
        short b = 10;
        int c = 15;
@@ -14,5 +15,6 @@ public class Main {
        System.out.println("Result - : " + (c - f));
        System.out.println("Result * : " + (b * s));
        System.out.println("Result / : " + (s / d));
+        
     }
 }
